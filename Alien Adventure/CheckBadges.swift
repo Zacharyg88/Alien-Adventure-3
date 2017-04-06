8419240fe934b86f8a6bfd24641bf3b39e1d5fc5
Zacharyg88 <zacharyg88@gmail.com>
2017-04-06 15:44:57 -0400

    Having Trouble with Check Badges

--- Alien Adventure/CheckBadges.swift
+++ Alien Adventure/CheckBadges.swift
@@ -9,7 +9,34 @@
 extension Hero {
     
     func checkBadges(badges: [Badge], requestTypes: [UDRequestType]) -> Bool {
-        return true
+        var correctBadgeCheck = Int()
+        var badgeCopy = [UDRequestType]()
+        for b in badges {
+            badgeCopy.append(b.requestType)
+        }
+            for r in requestTypes {
+                if badgeCopy.count >= 1 {
+                if badgeCopy.contains(r) {
+                    correctBadgeCheck += 1
+                   // print(badgeCopy)
+                    //badgeCopy.remove(at: 0)
+                    //print(badgeCopy)
+                }
+            }
+        }
+    
+        print(correctBadgeCheck)
+        print(badges.count)
+        if correctBadgeCheck == badges.count {
+            correctBadgeCheck = 0
+            return true
+        }else{
+            correctBadgeCheck = 0
+            return false
+        }
+        
+        
     }
     
 }
+
